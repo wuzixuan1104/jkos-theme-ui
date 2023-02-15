0f5408a37437ddce4bc1f832265f5ef7506668be
吳姿萱 <cherry51120@gmail.com>
2023-02-15 23:29:02 +0800

    fix: bubble

--- 02/assets/js/main.js
+++ 02/assets/js/main.js
@@ -3,29 +3,47 @@ import '../css/style.css'
 window.onload = function() {
   // 跑馬燈
   const el = document.getElementById('ticker');
-  let tickerHtml = ''
   const component = el.innerHTML;
-  for (let i = 0; i < 10; i ++) {
+  const trackerWidth = 500; // 一組 500px
+  const num = Math.ceil(window.screen.width / trackerWidth);
+
+  let tickerHtml = ''
+  for (let i = 0; i <= num; i ++)
     tickerHtml += component
-  }
+
   el.innerHTML = tickerHtml;
 
+  
   // 訊息泡泡
   const options = {
+    root: null,
     rootMargin: '0px 0px 50px 0px',
-    threshold: 0
+    threshold:  [0.9, 1.0]
   }
-  const message = document.querySelectorAll('.message-wrapper')
   
-  const callback = (entries, observer) => {
+  let intersectionRatio = undefined;
+
+  const callback = (entries) => {
     entries.forEach(entry => {
-      console.log(entry);
+      const currentRatio = intersectionRatio;
+      const newRatio = entry.intersectionRatio;
+      const boundingClientRect = entry.boundingClientRect;
+      const scrollingDown = currentRatio !== undefined && newRatio < currentRatio && boundingClientRect.bottom < boundingClientRect.height;
+
+      intersectionRatio = newRatio;
+
+      if (!entry.isIntersecting) {
+        if (!scrollingDown)
+          entry.target.querySelector('.message').classList.remove('open');
+        return;
+      }
+
       
-      if (!entry.isIntersecting) return
       entry.target.querySelector('.message').classList.add('open');
-      observer.unobserve(entry.target)
     })
   }
+
+  const message = document.querySelectorAll('.message-wrapper')
 
   let observer = new IntersectionObserver(callback, options);
   message.forEach((target) => {
